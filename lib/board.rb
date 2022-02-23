74eff82881eec1df71b248d0bb69cc6849d9520b
Pythonikk <nikkibrusoe@gmail.com>
2022-02-23 15:52:24 -0800

    initial commit

---
+++ lib/board.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'colorize'
+require 'colorized_string'
+
+# the game board
+class Board
+  def initialize; end
+
+  def display
+    4.times do
+      4.times do
+        print white_square, black_square
+      end
+      puts "\n"
+      4.times do
+        print black_square, white_square
+      end
+      puts "\n"
+    end
+  end
+
+  def white_square(occupied = false, piece = KNIGHT)
+    occupied_square = " #{piece}  ".colorize(:color => :black, :background => :white)
+    empty_square = '    '.colorize(:background => :white)
+
+    return occupied_square if occupied
+
+    empty_square
+  end
+
+  def black_square(occupied = false, piece = KNIGHT)
+    # piece.color
+    occupied_square = " #{piece}  ".colorize(:color => :white, :background => :light_black)
+    empty_square = '    '.colorize(:background => :light_black)
+
+    return occupied_square if occupied
+
+    empty_square
+  end
+
+  # TEMP for testing
+  KNIGHT = "\u{265E}".freeze
+end
