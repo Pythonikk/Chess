7dadf51bacfb49bd1e8d869b9f6c95629439cbca
Pythonikk <nikkibrusoe@gmail.com>
2022-02-25 11:26:22 -0800

    Rook has #moves

--- lib/rook.rb
+++ lib/rook.rb
@@ -2,12 +2,60 @@
 
 # defines the Rooks behavior
 class Rook
-  attr_reader :type, :color, :start_pos, :current_pos
+  attr_reader :type, :color, :start_pos, :current_pos,
+              :cp1, :cp2, :arr
 
   def initialize(type, color, pos)
     @type = type
     @color = color
     @pos = pos
     @current_pos = pos
+    abbreviate
+  end
+
+  def abbreviate
+    @cp1 = current_pos[0]
+    @cp2 = current_pos[1].to_i
+  end
+
+  def moves
+    @arr = []
+    north_moves
+    south_moves
+    row_left_moves
+    row_right_moves
+    arr
+  end
+
+  def north_moves
+    row = cp2
+    until row == 8
+      row += 1
+      arr << [cp1, row]
+    end
+  end
+
+  def south_moves
+    row = cp2
+    until row == 1
+      row -= 1
+      arr << [cp1, row]
+    end
+  end
+
+  def row_left_moves
+    column = cp1
+    until column == 'a'
+      column = Board.column(column, -1)
+      arr << [column, cp2]
+    end
+  end
+
+  def row_right_moves
+    column = cp1
+    until column == 'h'
+      column = Board.column(column, 1)
+      arr << [column, cp2]
+    end
   end
 end
