4802bc294a1e27b85c95be64227ca6604bb220c2
Pythonikk <nikkibrusoe@gmail.com>
2022-02-25 13:03:33 -0800

    Knight has #moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,12 +2,39 @@
 
 # defines the Knights behavior
 class Knight
-  attr_reader :type, :color, :start_pos, :current_pos
+  attr_reader :type, :color, :start_pos, :current_pos,
+              :cp1, :cp2, :squares
 
   def initialize(type, color, pos)
     @type = type
     @color = color
     @pos = pos
     @current_pos = pos
+    abbreviate
+  end
+
+  def abbreviate
+    @cp1 = current_pos[0]
+    @cp2 = current_pos[1].to_i
+  end
+
+  def moves
+    @squares = []
+
+    alterations.each do |a|
+      column = Board.column(cp1, a[0])
+      row = cp2 + a[1]
+      squares << [column, row]
+    end
+    format_squares
+  end
+
+  def alterations
+    [[1, 2], [2, 1], [2, -1], [1, -2],
+     [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+  end
+
+  def format_squares
+    squares.map! { |i| i.join('').to_sym }
   end
 end
